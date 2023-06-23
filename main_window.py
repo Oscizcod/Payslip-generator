5d840165a7fbc714a640e4a831a60e9725cd2cef
Pravin <pravin_muruhaya@hotmail.com>
2023-06-23 12:55:28 +0800

    Minimalise main window -Display menubar with 'Employees' and 'Payments' menus -Set QtWidget obj as central widget

--- main_window.py
+++ main_window.py
@@ -1,4 +1,4 @@
-from PySide6.QtWidgets import QMainWindow
+from PySide6.QtWidgets import QMainWindow, QWidget, QGridLayout, QLabel, QPushButton, QHBoxLayout, QVBoxLayout
 
 class MainWindow(QMainWindow):
     def __init__(self, app):
@@ -14,3 +14,13 @@ class MainWindow(QMainWindow):
         main_menu = self.menuBar()
         emp_menu = main_menu.addMenu('Employees')
         file_menu = main_menu.addMenu('Payment')
+
+        self.widget = QWidget()
+        self.setCentralWidget(self.widget)
+
+    def change_layout(self, new_layout):
+        self.widget.setLayout(new_layout)
+
+    def add_emp(self):
+        pass
+        
