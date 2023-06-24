93083ba296b57a028d9aaa6188ba04b8be933f68
Pravin <pravin_muruhaya@hotmail.com>
2023-06-24 18:58:47 +0800

    Edit file to use QStackedLayout for multipage window - Define a generic layout for central widget using QVBoxLayout - Define separate page layout for home page using new QWidget instance - Define stacked layout and add home page layout, payment invoice layout - Define function to change layout when menu action triggered - Define function to quit app when menu action triggered

--- main_window.py
+++ main_window.py
@@ -1,5 +1,6 @@
-from PySide6.QtWidgets import QMainWindow, QWidget, QGridLayout, QLabel, QPushButton, QHBoxLayout, QVBoxLayout
-from payment_mainLayout import PaymentLayout
+from PySide6.QtWidgets import QMainWindow, QWidget, QVBoxLayout, QLabel, QStackedLayout, QFormLayout, QLineEdit
+from payment_summaryLayout import PaymentInvoiceLayout
+from PySide6.QtCore import Slot
 
 class MainWindow(QMainWindow):
     def __init__(self, app):
@@ -11,26 +12,52 @@ class MainWindow(QMainWindow):
         # set window title
         self.setWindowTitle('Payslip generator')
 
+         # define generic widget to display main layout
+        layout_main = QVBoxLayout()
+        self.widget = QWidget()
+        self.widget.setLayout(layout_main)
+        self.setCentralWidget(self.widget)
+
+        # define home page layout
+        self.layout_home_page = QWidget()
+        self.layout_home_page.setLayout(QVBoxLayout())
+        label_homePage_title = QLabel()
+        label_homePage_title.setText('Klinik Muru Employees')
+        self.layout_home_page.layout().addWidget(label_homePage_title)
+
+        # define stacked layout
+        self.layout_stacked = QStackedLayout()
+        # add home page layout to stack - index 0
+        self.layout_stacked.addWidget(self.layout_home_page)
+        # add payment summary layout to stack - index 1
+        self.layout_pay_summary = PaymentInvoiceLayout()
+        self.layout_stacked.addWidget(self.layout_pay_summary)
+
+        # add stacked layout to main layout
+        layout_main.addLayout(self.layout_stacked) 
+        self.layout_stacked.setCurrentIndex(0)
+
         # set up our menu bar
         main_menu = self.menuBar()
+        file_menu = main_menu.addMenu('File')
+        action_file_home = file_menu.addAction('Home')
+        action_file_quit = file_menu.addAction('Quit')
+
         emp_menu = main_menu.addMenu('Employees')
 
         payment_menu = main_menu.addMenu('Payment')
         action_pay_summary = payment_menu.addAction('Summary')
-        action_pay_summary.triggered.connect(self.payment_action_clicked)
+        
 
-        self.widget = QWidget()
-        self.setCentralWidget(self.widget)
+        action_file_home.triggered.connect(lambda: self.change_layout(0))
+        action_file_quit.triggered.connect(self.action_file_quit_clicked)
+        action_pay_summary.triggered.connect(lambda: self.change_layout(1))
 
-    def change_layout(self, new_layout):
-        self.widget.setLayout(new_layout)
+    @Slot()
+    def change_layout(self, index):
+        self.layout_stacked.setCurrentIndex(index)
 
-    def add_emp(self):
-        pass
-
-    def payment_action_clicked(self):
-        payment_mainLayout = PaymentLayout()
-        self.change_layout(payment_mainLayout)
-
+    def action_file_quit_clicked(self):
+        self.app.quit()
 
         
