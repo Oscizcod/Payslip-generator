49fc40740268ee597aed1269da62bb23161160e9
Pravin <pravin_muruhaya@hotmail.com>
2023-06-30 01:44:37 +0800

    'Generate payslip' button now opens dialog asking for all relevant inputs to compute employee's pay

--- payment_invoiceLayout.py
+++ payment_invoiceLayout.py
@@ -1,37 +1,137 @@
-from PySide6.QtWidgets import QWidget,QVBoxLayout, QLabel, QHBoxLayout, QFileDialog, QApplication, QLineEdit, QPushButton, QDialog
+from PySide6.QtWidgets import QVBoxLayout, QLabel, QHBoxLayout, QFileDialog, QLineEdit, QPushButton, QDialog, QFormLayout, QGroupBox, QComboBox, QCheckBox
 from PySide6.QtCore import Slot
 
 class PaymentInvoiceLayout(QDialog):
     def __init__(self):
         super().__init__()
 
-        # add label for file upload
-        label_attn_file_upload = QLabel()
-        label_attn_file_upload.setText('Please upload your attendance file:')
+        # initialise UI
+        self.initUI()
 
-        # add input box for file upload
-        # plus browse button
-        layout_attn_upload = QHBoxLayout()
+        # connect btn signals to slots
+        self.btn_browse_attn_file.clicked.connect(self.btn_browse_attn_file_clicked)
+        self.btn_gen_payment_invoice.clicked.connect(self.btn_ok_clicked)
+    
+    def initUI(self):
+        # inputs
+        self.btn_gen_payment_invoice = QPushButton()
+        self.btn_gen_payment_invoice.setText('OK')
+        self.btn_save_emp_details = QPushButton()
+        self.btn_save_emp_details.setText('Save')
+        self.btn_cancel = QPushButton()
+        self.btn_cancel.setText('Cancel')
+
+        # define layouts
+        # horizontal layout for both btns
+        layout_btns = QHBoxLayout()
+        layout_btns.addWidget(self.btn_save_emp_details)
+        layout_btns.addWidget(self.btn_gen_payment_invoice)
+        layout_btns.addWidget(self.btn_cancel)
+        layout_top_main = QVBoxLayout()
+        layout_top_main.addWidget(self.workingUI())
+        layout_top_main.addWidget(self.paymentUI())
+        layout_top_main.addWidget(self.attnFileUI())
+        layout_top_main.addLayout(layout_btns)
+        self.setLayout(layout_top_main)
+
+    def attnFileUI(self):
+        # all components for upload attendance file
         self.input_attn_url = QLineEdit()  # accept file path url
-        layout_attn_upload.addWidget(self.input_attn_url)
-        btn_browse_attn_file = QPushButton()
-        btn_browse_attn_file.setText('Browse')
-        btn_browse_attn_file.clicked.connect(self.btn_browse_attn_file_clicked)
-        layout_attn_upload.addWidget(btn_browse_attn_file)
-
-        # add push button for generating payment invoice
-        btn_gen_payment_invoice = QPushButton()
-        btn_gen_payment_invoice.setText('OK')
-        btn_gen_payment_invoice.clicked.connect(self.btn_ok_clicked)
-
-        # add all widgets and layouts to global layout
-        self.setLayout(QVBoxLayout())
-        self.layout().addWidget(label_attn_file_upload)
-        layout_attn_upload.addWidget(self.input_attn_url)
-        layout_attn_upload.addWidget(btn_browse_attn_file)
-        self.layout().addLayout(layout_attn_upload)
-        self.layout().addWidget(btn_gen_payment_invoice)
+        self.btn_browse_attn_file = QPushButton()
+        self.btn_browse_attn_file.setText('Browse')
         
+        # layout
+        frame_upload = QGroupBox('Attendance file upload')
+        layout_attn_upload = QHBoxLayout()
+        layout_attn_upload.addWidget(self.input_attn_url)
+        layout_attn_upload.addWidget(self.btn_browse_attn_file)
+        frame_upload.setLayout(layout_attn_upload)
+
+        return frame_upload
+
+    def paymentUI(self):
+        # define all inputs
+        edit_base_pay = QLineEdit()
+        edit_epf_employer = QLineEdit()
+        edit_epf_employee = QLineEdit()
+        edit_charge_overtime = QLineEdit()
+        edit_charge_late_arr = QLineEdit()
+        edit_charge_early_dep = QLineEdit()
+
+        # create layout
+        # use form for all inputs
+        layout_payment = QFormLayout()
+        layout_payment.addRow('Base pay: RM', edit_base_pay)
+        layout_payment.addRow('EPF Employer contribution: RM', edit_epf_employer)
+        layout_payment.addRow('EPF Employee contribution: RM', edit_epf_employee)
+        layout_payment.addRow('Overtime charges: RM', edit_charge_overtime)
+        layout_payment.addRow('Late arrival charges: RM', edit_charge_late_arr)
+        layout_payment.addRow('Early departure charges: RM', edit_charge_early_dep)
+
+        # add form to groupbox
+        frame_payment = QGroupBox('Payment info')
+        frame_payment.setLayout(layout_payment)
+        
+        return frame_payment
+
+    def workingUI(self):
+        # define constants
+        WORKING_HOURS = self.generate_working_hrs()
+        WORKING_DAYS = ['Monday', 'Tuesday', 'Wednesday', 'Thursday',
+                        'Friday', 'Saturday', 'Sunday']
+
+        # define all inputs
+        edit_start_time = QComboBox()
+        edit_start_time.addItems(WORKING_HOURS)
+        edit_end_time = QComboBox()
+        edit_end_time.addItems(WORKING_HOURS)
+        # add options for checkbox
+        edit_work_mon = QCheckBox(WORKING_DAYS[0])
+        edit_work_tue = QCheckBox(WORKING_DAYS[1])
+        edit_work_wed = QCheckBox(WORKING_DAYS[2])
+        edit_work_thurs = QCheckBox(WORKING_DAYS[3])
+        edit_work_fri = QCheckBox(WORKING_DAYS[4])
+        edit_work_sat = QCheckBox(WORKING_DAYS[5])
+        edit_work_sun = QCheckBox(WORKING_DAYS[6])
+
+        # create layout
+        # put start and end times horizontally
+        layout_timing = QHBoxLayout()
+        label_start_time = QLabel()
+        label_start_time.setText('Start time: ')
+        label_end_time = QLabel()
+        label_end_time.setText('End time: ')
+        layout_timing.addWidget(label_start_time)
+        layout_timing.addWidget(edit_start_time)
+        layout_timing.addWidget(label_end_time)
+        layout_timing.addWidget(edit_end_time)
+        # put all in a group box with vertical layout
+        frame_working = QGroupBox('Working info')
+        layout_top_working = QVBoxLayout()
+        frame_working.setLayout(layout_top_working)
+        label_working_days = QLabel()
+        label_working_days.setText('Select working days: ')
+        frame_working.layout().addLayout(layout_timing)
+        frame_working.layout().addWidget(label_working_days)
+        frame_working.layout().addWidget(edit_work_mon)
+        frame_working.layout().addWidget(edit_work_tue)
+        frame_working.layout().addWidget(edit_work_wed)
+        frame_working.layout().addWidget(edit_work_thurs)
+        frame_working.layout().addWidget(edit_work_fri)
+        frame_working.layout().addWidget(edit_work_sat)
+        frame_working.layout().addWidget(edit_work_sun)
+
+        return frame_working
+
+    def generate_working_hrs(self):
+        working_hrs = []
+
+        for hr in range(7, 23):
+            working_hrs.append(str(hr) + ':00')
+            working_hrs.append(str(hr) + ':30')
+
+        return working_hrs
+    
     @Slot()
     def btn_browse_attn_file_clicked(self):
         file_dialog = QFileDialog()
@@ -45,18 +145,3 @@ class PaymentInvoiceLayout(QDialog):
         # TODO: check to see if acceptable input
         # TODO: where to implement layout change
         pass
-
-
-# TODO: DELETE below code once integrated into whole app
-
-if __name__ == '__main__':
-    # initialise the event loop listener
-    app = QApplication()
-    # initialise main window and show
-    main_window = QWidget()
-    layout = PaymentInvoiceLayout()
-    main_window.setLayout(layout)
-    main_window.show()
-
-    # start event loop listener
-    app.exec()
